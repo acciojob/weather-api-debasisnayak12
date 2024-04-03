03a171a19e4cd5a247cfad9c7b27f1257f78900a
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2024-04-03 12:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+const API_KEY = "ed4d31dd300b25a056d846b625caf1b0";
+btn.addEventListener('click',()=>{
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`)
+	.then((res)=>res.json())
+	.then(data => {
+		let weatherD = document.getElementById("weatherData");
+		weatherD.innerHTML = `Current weather in London: ${data.weather[0].description}`;
+	})
+	.catch((error)=>console.log(error));
+})
+
